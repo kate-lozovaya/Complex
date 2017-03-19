ab476540890bdf2634d82996cd56228c57bbd6a8
kate-lozovaya <katekalozovaya@gmail.com>
2017-03-19 21:52:16 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,15 +4,15 @@
 SCENARIO("complex init", "[init]") 
 {
 	TComplex complex;
-	REQUIRE(complex.real() == 0);
-	REQUIRE(complex.imaginary() == 0);
+	REQUIRE(complex.real_() == 0);
+	REQUIRE(complex.imaginary_() == 0);
 }
 
 SCENARIO("complex with params", "[init]") 
 {
 	TComplex complex(2,3);
-	REQUIRE(complex.real() == 2);
-	REQUIRE(complex.imaginary() == 3);
+	REQUIRE(complex.real_() == 2);
+	REQUIRE(complex.imaginary_() == 3);
 }
 
 SCENARIO("*", "[init]") 
